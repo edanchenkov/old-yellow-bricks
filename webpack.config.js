4774bfbcf622268b67aa89c076f319eeac5d0b73
Egor Danchenkov <icepnyx@gmail.com>
2017-03-19 21:22:48 +0200

    Add gzip build for future references

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
     filename : 'bundle.css'
-    // disable : process.env.NODE_ENV === 'dddevelopment'
 });
 
 module.exports = {
@@ -55,6 +55,13 @@ module.exports = {
         new CopyWebpackPlugin([
             { from : 'app/images', to : 'images', ignore : ['.DS_Store'] },
             { from : 'index.html', to : 'index.html' },
-        ])
+        ]),
+        new CompressionPlugin({
+            asset : "[path].gz[query]",
+            algorithm : "gzip",
+            test : /\.(js|html)$/,
+            threshold : 10240,
+            minRatio : 0.8
+        })
     ]
 };
